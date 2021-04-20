ca1ea9f7623748f857a39c379611382271f1d080
Daniel Rumfelt <76234995+wxdaniel89@users.noreply.github.com>
2021-04-20 18:10:10 -0400

    User Methods/Login Updated

--- src/edu/gcu/cst341/project/MyStore.java
+++ src/edu/gcu/cst341/project/MyStore.java
@@ -1,16 +1,17 @@
 package edu.gcu.cst341.project;
 
-import java.beans.Statement;
-import java.sql.Connection;
+import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class MyStore {
-
+	static Scanner sc = new Scanner(System.in);
 	private String name;
 	private DBConnect con;
+	private int userID;
+	private String customer;
 
 	MyStore(String name) {
 		this.name = name;
@@ -23,20 +24,17 @@ public class MyStore {
 		do {
 			switch (UserInterface.menuMain()) {
 			case 0:
-				System.out.println("Thank you! Come again!");
+				System.out.println("\nThank you! Come again!");
 				exit = true;
 				break;
 			case 1:
 				user = login();
 				if (user != null) {
-					System.out.println("Login successful!!");
+					System.out.println("Login successful!!\n");
 					shop();
 				} else {
-					System.out.println("Login unsuccessful");
+					System.out.println("Login unsuccessful\n");
 				}
-				break;
-			case 2:
-				admin();
 				break;
 			default:
 				open();
@@ -45,19 +43,25 @@ public class MyStore {
 	}
 
 	private String login() {
+
 		String result = null;
 
 		String[] login = UserInterface.login();
-
-		String sql = "SELECT UserId, UserFirstName FROM users WHERE UserName = ? AND UserPassword = ? AND UserStatus = 1";
-
+		
+//		Altered the select statement to match the database. 
+//		Daniel Rumfelt 4/19/2021
+		String sql = "SELECT user_Id, user_First_Name FROM users WHERE user_Name = ? AND user_Password = ? AND user_Status = \"Active\"";
 		try (PreparedStatement ps = con.getConnection().prepareStatement(sql)) {
 			ps.setString(1, login[0]);
 			ps.setString(2, login[1]);
 			ResultSet rs = ps.executeQuery();
 
 			if (rs.next()) {
-				result = rs.getString("UserFirstName");
+				userID = rs.getInt("user_Id");
+				customer = rs.getString("user_First_Name");
+				result = rs.getString("user_First_Name");
+				
+				System.out.println("\nThank you, " + customer + " you have successfully logged in.");
 			} else {
 				result = null;
 			}
@@ -66,13 +70,19 @@ public class MyStore {
 		}
 		return result;
 	}
+	
 
 	private void shop() {
 		switch (UserInterface.menuShop()) {
 		case 0:
 			return;
 		case 1:
-			createCartItem();
+			try {
+				createCartItem();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			break;
 		case 2:
 			readCartItems();
@@ -126,24 +136,108 @@ public class MyStore {
 		}
 	}
 
-	private void createCartItem() {
+	private void createCartItem() throws SQLException {
+//		Added Method 4-18-2021
+//		Bruce Brown
+		
 		System.out.println("Add (Create) item to cart...");
-		System.out.println();
+		readProducts();
+		System.out.println("What is the product ID of the item you wish to add?");
+		int item = sc.nextInt();
+		sc.nextLine();
+		System.out.println("How Many Would You Like to Add?");
+		int quantity = sc.nextInt();
+		sc.nextLine();
+		for (int i = 0; i < quantity; i++) {
+			String sqlInsert = "INSERT INTO cst341nproject.shopping_cart (user_Id, product_Id) VALUES (?, ?)";
+			try (PreparedStatement ps = con.getConnection().prepareStatement(sqlInsert)) {
+				ps.setInt(1, userID);
+				ps.setInt(2, item);
+				ps.executeUpdate();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Item ID: " + item + " Was Added " + quantity + " Time(s)!");
+		System.out.println("\nPress \"ENTER\" to continue...\n");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		shop();
 	}
+
 
 	private void readCartItems() {
-		System.out.println("View (Read) cart...");
-		System.out.println();
+//		Added Method 4-18-2021
+//		Daniel Rumfelt
+//		
+		System.out.println("View (Read) cart...\n");
+		System.out.println(customer + ", Items currently in your shopping cart.");
+		String sql = "SELECT shopping_cart.product_Id, products.product_Name, products.product_Price\n"
+				+ "FROM cst341nproject.products\n"
+				+ "JOIN  cst341nproject.shopping_cart on shopping_cart.product_Id = products.product_Id\n"
+				+ "WHERE shopping_cart.user_Id = ?";
+		try (PreparedStatement ps = con.getConnection().prepareStatement(sql)) {
+			ps.setInt(1, userID);
+			ResultSet rs = ps.executeQuery();
+			System.out.println("Product ID   Product Name     Product Price");
+			System.out.println("--------------------------------------------");
+			Double total = 0.00;
+			while (rs.next()) {
+				System.out.printf("%-12s %-15s  $%,.2f\n",rs.getInt("product_Id"),rs.getString("product_Name"),
+						rs.getDouble("product_Price"));
+				
+				total = total + rs.getDouble("product_Price");
+			}
+			System.out.println("--------------------------------------------");
+			System.out.printf("%-29s $%,.2f\n","Total", total);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("\nPress \"ENTER\" to continue...\n");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+
 
 	private void deleteCartItem() {
-		Scanner sc = new Scanner(System.in);
-
+		
+//		Added Method 4-18-2021
+//		Bruce Brown
+		
 		System.out.println("Delete from cart...");
-		System.out.println();
+		System.out.println("Choose from the following items to delete:");
+		readCartItems();
+		System.out.println("Type the Product ID of the item you wish to remove, and press enter.");
+		int id = sc.nextInt();
+		sc.nextLine();
+		String sql = "DELETE FROM cst341nproject.shopping_cart WHERE product_Id = ? and user_Id = ? ";
+		try (PreparedStatement ps = con.getConnection().prepareStatement(sql)) {
+			ps.setInt(1, id);
+			ps.setInt(2, userID);
+			ps.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Deleting " + id + " from Your Cart...\n");
+		System.out.println("\nPress \"ENTER\" to continue...\n");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		shop();
 	}
+	
 
 	private void createProduct() throws SQLException {
+//		Added Method 4-18-2021
+//		Daniel Rumfelt
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Create product...");
--- src/edu/gcu/cst341/project/UserInterface.java
+++ src/edu/gcu/cst341/project/UserInterface.java
@@ -8,12 +8,13 @@ import java.util.Scanner;
 public class UserInterface {
 
 	static Scanner sc = new Scanner(System.in);
-
+//	Updated layout and inspect 4-19-2021
+	
 	public static String [] login() {
 		
 		String [] login = new String[2];
 		
-		System.out.println("LOG-IN");
+		System.out.println("\nLOG-IN");
 		System.out.println("==================");
 		System.out.println("Enter username: ");
 		login[0] = sc.nextLine();
@@ -24,10 +25,11 @@ public class UserInterface {
 		return login;
 	}
 	
+	
 	public static int menuMain() {
 		int option = 0;
 		do {
-			System.out.println("Main MENU");
+			System.out.println("\nMain MENU");
 			System.out.println("==================");
 			System.out.println("0] Exit");
 			System.out.println("1] Shop");
@@ -38,37 +40,41 @@ public class UserInterface {
 		} while (option < 0 || option > 2);
 		return option;
 	}
+	
 
 	public static int menuShop() {
 		int option = 0;
 		do {
-			System.out.println("Shopping MENU");
+			System.out.println("\nShopping MENU");
 			System.out.println("==================");
 			System.out.println("0] Back to Main Menu");
-			System.out.println("1] Add items to cart");
-			System.out.println("2] See items in cart");
-			System.out.println("3] Remove all items from cart");
+			System.out.println("1] Browse Inventory");
+			System.out.println("2] Add items to cart");
+			System.out.println("3] See items in cart");
+			System.out.println("4] Remove all items from cart");
 			System.out.println("==================");
 			option = sc.nextInt();
 			sc.nextLine();
-		} while (option < 0 || option > 3);
+		} while (option < 0 || option > 4);
 		return option;
 	}
 	
+
 	public static int menuAdmin() {
 		int option = 0;
 		do {
-			System.out.println("Administration MENU");
+			System.out.println("\nAdministration MENU");
 			System.out.println("==================");
 			System.out.println("0] Back to Main Menu");
 			System.out.println("1] Add product");
 			System.out.println("2] See all products");
 			System.out.println("3] Update a product");
-			System.out.println("4] Remove a product");			
+			System.out.println("4] Remove a product");
+			System.out.println("5] Change a product's availability");
 			System.out.println("==================");
 			option = sc.nextInt();
 			sc.nextLine();
-		} while (option < 0 || option > 4);
+		} while (option < 0 || option > 5);
 		return option;
 	}
 
